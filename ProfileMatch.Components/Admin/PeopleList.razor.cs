505c982c02d85b32589fee22de687c063b12d2aa
Paweł Przytuła <paprzytula@gmail.com>
2021-09-26 12:17:29 +0200

    added people list component

---
+++ ProfileMatch.Components/Admin/PeopleList.razor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Components;
+
+using ProfileMatch.Contracts;
+using ProfileMatch.Models.Models;
+
+namespace ProfileMatch.Components.Admin
+{
+    public partial class PeopleList : ComponentBase
+    {
+        [Inject]
+        public IUserService UserService { get; set; }
+
+        [Inject]
+        private NavigationManager NavMan { get; set; }
+
+        public string SearchString { get; set; }
+
+        private bool _dense = false;
+        public bool disabled = false;
+
+        private bool FilterFunc1(ApplicationUser person) => FilterFunc(person, SearchString);
+
+        private static bool FilterFunc(ApplicationUser person, string searchString)
+        {
+            if (string.IsNullOrWhiteSpace(searchString))
+                return true;
+            //if (person.Department.Name.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+            //    return true;
+            if (person.FirstName.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+                return true;
+            if (person.LastName.Contains(searchString, StringComparison.OrdinalIgnoreCase))
+                return true;
+            if ($"{person.DateOfBirth} {person.IsActive}".Contains(searchString))
+                return true;
+            return false;
+        }
+
+        public List<ApplicationUser> Users { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Users = await UserService.GetAllUsers();
+        }
+
+        private void ShowProfile(string id)
+        {
+            NavMan.NavigateTo($"/admin/users/{id}");
+        }
+
+        private void EditProfile(string id)
+        {
+            NavMan.NavigateTo($"/admin/edituser/{id}");
+        }
+    }
+}
