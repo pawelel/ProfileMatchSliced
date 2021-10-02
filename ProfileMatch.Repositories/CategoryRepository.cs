18862667a22b113b94c55d1ec496e320cfcad19f
Paweł Przytuła <paprzytula@gmail.com>
2021-10-03 00:17:35 +0200

    updated category repository

--- ProfileMatch.Repositories/CategoryRepository.cs
+++ ProfileMatch.Repositories/CategoryRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.EntityFrameworkCore;
@@ -30,34 +31,54 @@ namespace ProfileMatch.Repositories
             return await repositoryContext.Categories.AsNoTracking().ToListAsync();
         }
 
-        public Task<Category> Create(Category category)
+        public async Task<Category> Create(Category category)
         {
-            throw new System.NotImplementedException();
+            using ApplicationDbContext repositoryContext = contextFactory.CreateDbContext();
+            var data = await repositoryContext.Categories.AddAsync(category);
+            await repositoryContext.SaveChangesAsync();
+            return data.Entity;
         }
 
-        public Task<Category> Delete(Category category)
+        public async Task<Category> Delete(Category category)
         {
-            throw new System.NotImplementedException();
+            using ApplicationDbContext repositoryContext = contextFactory.CreateDbContext();
+            var data = repositoryContext.Categories.Remove(category).Entity;
+            await repositoryContext.SaveChangesAsync();
+            return data;
         }
 
-        public Task<Category> FindByName(string categoryName)
+        public async Task<Category> FindByName(string categoryName)
         {
-            throw new System.NotImplementedException();
+            using ApplicationDbContext repositoryContext = contextFactory.CreateDbContext();
+            return await repositoryContext.Categories.SingleOrDefaultAsync(c => c.Name == categoryName);
         }
 
-        public Task<Category> FindById(int categoryId)
+        public async Task<Category> FindById(int categoryId)
         {
-            throw new System.NotImplementedException();
+            using ApplicationDbContext repositoryContext = contextFactory.CreateDbContext();
+            return await repositoryContext.Categories.SingleOrDefaultAsync(c => c.Id == categoryId);
         }
 
-        public Task<List<Category>> GetAll()
+        public async Task<List<Category>> GetAll()
         {
-            throw new System.NotImplementedException();
+            using ApplicationDbContext repositoryContext = contextFactory.CreateDbContext();
+            return await repositoryContext.Categories.ToListAsync();
         }
 
-        public Task<Category> Update(Category category)
+        public async Task<Category> Update(Category category)
         {
-            throw new System.NotImplementedException();
+            using ApplicationDbContext repositoryContext = contextFactory.CreateDbContext();
+            var existing = await repositoryContext.Categories.FindAsync(category.Id);
+            if (existing != null)
+            {
+                repositoryContext.Entry(existing).CurrentValues.SetValues(category);
+                await repositoryContext.SaveChangesAsync();
+                return existing;
+            }
+            else
+            {
+                return category;
+            }
         }
     }
 }
