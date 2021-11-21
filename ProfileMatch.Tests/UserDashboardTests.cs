5ff6799ec4d1c08b23605d225a4e1dbb095d999b
Paweł Przytuła <paprzytula@gmail.com>
2021-11-21 15:30:07 +0100

    renamed test class

---
+++ ProfileMatch.Tests/UserDashboardTests.cs
@@ -0,0 +1,18 @@
+using Bunit;
+
+using ProfileMatch.Sites;
+
+using Xunit;
+
+namespace ProfileMatch.Tests
+{
+    public class UserDashboardTests : TestContext
+    {
+        [Fact]
+        public void Test1()
+        {
+            var cut = RenderComponent<UserDashboard>();
+            cut.Find()
+        }
+    }
+}
