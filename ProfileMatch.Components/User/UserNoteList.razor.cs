7656275f3b785b9daf28d87525a648c917d3d665
Paweł Przytuła <paprzytula@gmail.com>
2021-11-22 23:19:57 +0100

    fixed name of UserLoginDisplay class

---
+++ ProfileMatch.Components/User/UserNoteList.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ProfileMatch.Components.User
+{
+    public partial class UserNoteList
+    {
+        public UserNoteList()
+        {
+
+        }
+    }
+}
