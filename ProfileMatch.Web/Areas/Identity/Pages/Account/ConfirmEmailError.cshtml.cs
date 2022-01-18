d65d6954791856ef6afee072191fa7792ce1cc9c
Łukasz <pzx90954@student.wsb.poznan.pl>
2022-01-18 21:05:38 +0100

    Email confirm and error

---
+++ ProfileMatch.Web/Areas/Identity/Pages/Account/ConfirmEmailError.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
+
+using ProfileMatch.Models.Models;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfileMatch.Web.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class ConfirmEmailErrorModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ConfirmEmailErrorModel(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+  
+    }
+}
